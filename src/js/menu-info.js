05129addf507f025a93a4af65863e793e6cbfdb9
Joshua Bradford <jjyet@yahoo.com>
2017-02-22 16:45:36 -0500

    added scss module and framework of functions

--- src/js/menu-info.js
+++ src/js/menu-info.js
@@ -5,12 +5,23 @@ function writeMenuInfo(){
     <div class="our-story">Our Story</div>
     <div class="menu">Menu</div>
     <div class="reservations">Reservations<div>
-  `)
-  $(".our-story").click(writeOurStory)
-  $(".menu").click(writeMenu)
-  $("reservations").click(writeReservations)
+  `);
+  $(".our-story").click(writeOurStory);
+  $(".menu").click(writeMenu);
+  $("reservations").click(writeReservations);
+}
+
+function writeOurStory(event){
+  
+}
+
+function writeMenu(event){
+
+}
+
+function writeReservations(event){
+
 }
 
 
-
-export { writeMenuInfo }
+export { writeMenuInfo };

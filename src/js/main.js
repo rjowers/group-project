221cce18c4d97cfcda05b32592551dc14d9d3a0a
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-23 13:51:18 -0500

    added second blurb with css styling and placeholder image

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import {maketopbar} from "./topbar.js"
 import {getNews} from "./newsblurb.js"
 import {makenewsblurb} from "./newsblurb.js"
+import {getFood} from "./todayblurb.js"
+import {putFood} from "./todayblurb.js"
 
 maketopbar();

1ce141740363f3227d8891934bdbc0e293973e09
Joshua Bradford <jjyet@yahoo.com>
2017-02-23 11:46:28 -0500

    more work on loop menu

--- src/js/menu-info.js
+++ src/js/menu-info.js
@@ -41,24 +41,40 @@ function getMenu () {
 }
 
 function printMenu(data){
+
+
   $(".menu-info-container").html(`
     <div>Beers, Food, and Games</div>
-    <div class="divide-line"></div>
+    <div class="divide-line1"></div>
     <div class="menu-section-title">Beer</div>
-    <div class="divide-line"></div>
-    <div class="menu-item+menu-price-container">
-      <div class="menu-item">${data.Beer[0].item}</div>
-      <div class="menu-item-price">${data.Beer[0].price}</div>
-    </div>
-    <div class="menu-item-description">${data.Beer[0].style}</div>
-    <div class="menu-item-description">${data.Beer[0].description}</div>
-    <div class="menu-item-description">${data.Beer[0].allergies}</div>
-    <div class="menu-item-description">${data.Beer[0].favorite}</div>
-
-
-
-
+    <div class="divide-line2"></div>
   `);
+  for(var count = 0; count < data.Beer.length; count++){
+    $(".divide-line2").after(`
+    <div class="menu-item-menu-price-container">
+      <div class="menu-item">${data.Beer[count].item}</div>............................
+      <div class="menu-item-price">${data.Beer[count].price}</div>
+    </div>
+    <div class="menu-item-style">${data.Beer[count].style}</div>
+    <div class="menu-description-allergies-favorite-container">
+      <div class="menu-item-description float-left">${data.Beer[0].description}</div>...
+      <div class="allergies-favorite float-right"></div>
+    </div>
+    `);
+
+
+    if(data.Beer[count].allergies === 1){
+      $(".allergies-favorite").html(`
+      <i class="float-right fa fa-exclamation-triangle" aria-hidden="true"></i>`);
+    }
+
+    if(data.Beer[count].favorite === 1){
+      var currentHtml = $(".allergies-favorite").html()
+      currentHtml += `<i class="float-right fa fa-star-o" aria-hidden="true"></i>`;
+      $(".allergies-favorite").html(currentHtml);
+    }
+
+  }
   console.log(data);
 }
 
